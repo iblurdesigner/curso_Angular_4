532b31a4950184b4ceaf3da5c482d638a028ea61
“iblurdesigner” <nevomen@gmail.comgit config --global user.emailgit config --global user.email nevomen@gmail.com>
2018-09-03 18:11:19 -0500

    Directivas propias de atributo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'platzisquare';
+  title = 'Platzisquare';
+  lugares:any = [
+    {plan: 'pagado', cercania: 1, distancia: 1, active: false, nombre: 'Floreria'},
+    {plan: 'gratuito', cercania: 1, distancia: 1.8, active: true, nombre: 'Doneria'},
+    {plan: 'gratuito', cercania: 2, distancia: 10, active: true, nombre: 'Veterinaria'},
+    {plan: 'pagado', cercania: 3, distancia: 35, active: false, nombre: 'Restaurante'},
+    {plan: 'gratuito', cercania: 3, distancia: 120, active: true, nombre: 'Mercado'}
+  ];
+  lat:number = -0.261479;
+  lng:number = -78.5593177;
+  constructor(){
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import { AgmCoreModule } from '@agm/core';
+import {ResaltarDirective} from './directives/resaltar.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResaltarDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAKK4TB9Cwn_XbTN3ZEP3_RyqoNRpVjdq8'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
