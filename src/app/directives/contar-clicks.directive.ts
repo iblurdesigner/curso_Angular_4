1240020eaff50917999c03e26ba33e285861ebff
“iblurdesigner” <nevomen@gmail.comgit config --global user.emailgit config --global user.email nevomen@gmail.com>
2018-09-03 19:30:58 -0500

    Host biding

--- src/app/directives/contar-clicks.directive.ts
+++ src/app/directives/contar-clicks.directive.ts
@@ -1,12 +1,14 @@
-import {Directive, HostListener} from '@angular/core';
+import {Directive, HostBinding, HostListener} from '@angular/core';
 
 @Directive({
-  selector: 'a[contar-clicks]'
+  selector: 'li[contar-clicks]'
 })
 
 export class ContarClicksDirective{
   clickN = 0;
+  @HostBinding('style.opacity') opacity:number = .1;
   @HostListener('click', ['$event.target']) onClick(btn){
     console.log('a', btn, "Numero de clicks:", this.clickN++);
+    this.opacity += .1;
   }
 }
