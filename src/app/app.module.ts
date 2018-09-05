fc841b0a2faf4df6c6bb88d85a78c349d0a5eba1
“iblurdesigner” <nevomen@gmail.comgit config --global user.emailgit config --global user.email nevomen@gmail.com>
2018-09-05 09:02:31 -0500

    Parametros en Rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import {LugaresComponent} from './lugares/lugares.component';
 const appRoutes: Routes = [
   {path: '', component: LugaresComponent},
   {path: 'lugares', component: LugaresComponent}
-  {path: 'detalle', component: DetalleComponent}
+  {path: 'detalle/:id', component: DetalleComponent}
 ]
 
 @NgModule({
--- src/app/lugares/lugares.component.ts
+++ src/app/lugares/lugares.component.ts
@@ -7,11 +7,11 @@ import { Component } from '@angular/core';
 export class LugaresComponent {
   title = 'Platzisquare';
   lugares:any = [
-    {plan: 'pagado', cercania: 1, distancia: 1, active: false, nombre: 'Floreria'},
-    {plan: 'gratuito', cercania: 1, distancia: 1.8, active: true, nombre: 'Doneria'},
-    {plan: 'gratuito', cercania: 2, distancia: 10, active: true, nombre: 'Veterinaria'},
-    {plan: 'pagado', cercania: 3, distancia: 35, active: false, nombre: 'Restaurante'},
-    {plan: 'gratuito', cercania: 3, distancia: 120, active: true, nombre: 'Mercado'}
+    {id: 1, plan: 'pagado', cercania: 1, distancia: 1, active: false, nombre: 'Floreria'},
+    {id: 2, plan: 'gratuito', cercania: 1, distancia: 1.8, active: true, nombre: 'Doneria'},
+    {id: 3, plan: 'gratuito', cercania: 2, distancia: 10, active: true, nombre: 'Veterinaria'},
+    {id: 4, plan: 'pagado', cercania: 3, distancia: 35, active: false, nombre: 'Restaurante'},
+    {id: 5, plan: 'gratuito', cercania: 3, distancia: 120, active: true, nombre: 'Mercado'}
   ];
   lat:number = -0.261479;
   lng:number = -78.5593177;
