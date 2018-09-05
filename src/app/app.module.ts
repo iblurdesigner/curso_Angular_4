bfb142d91613defb098ab66785ef390c662294d5
“iblurdesigner” <nevomen@gmail.comgit config --global user.emailgit config --global user.email nevomen@gmail.com>
2018-09-05 10:02:18 -0500

    Creando una vista de detalle para el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {FormsModule} from "@angular/forms";
+import {FormsModule} from '@angular/forms';
 import { AgmCoreModule } from '@agm/core';
 import {ResaltarDirective} from './directives/resaltar.directive';
 import {ContarClicksDirective} from './directives/contar-clicks.directive';
@@ -13,9 +13,9 @@ import {LugaresComponent} from './lugares/lugares.component';
 
 const appRoutes: Routes = [
   {path: '', component: LugaresComponent},
-  {path: 'lugares', component: LugaresComponent}
+  {path: 'lugares', component: LugaresComponent},
   {path: 'detalle/:id', component: DetalleComponent}
-]
+];
 
 @NgModule({
   declarations: [
--- src/app/detalle/detalle.component.ts
+++ src/app/detalle/detalle.component.ts
@@ -6,9 +6,23 @@ import {ActivatedRoute} from '@angular/router';
   templateUrl: './detalle.component.html'
 })
 export class DetalleComponent {
+  lugares: any = [
+    {id: 1, plan: 'pagado', cercania: 1, distancia: 1, active: false, nombre: 'Floreria', description: 'Descripcion de este negocio. Más adelante tendremos mas informacion'},
+    {id: 2, plan: 'gratuito', cercania: 1, distancia: 1.8, active: true, nombre: 'Doneria', description: 'Descripcion de este negocio. Más adelante tendremos mas informacion'},
+    {id: 3, plan: 'gratuito', cercania: 2, distancia: 10, active: true, nombre: 'Veterinaria', description: 'Descripcion de este negocio. Más adelante tendremos mas informacion'},
+    {id: 4, plan: 'pagado', cercania: 3, distancia: 35, active: false, nombre: 'Restaurante', description: 'Descripcion de este negocio. Más adelante tendremos mas informacion'},
+    {id: 5, plan: 'gratuito', cercania: 3, distancia: 120, active: true, nombre: 'Mercado', description: 'Descripcion de este negocio. Más adelante tendremos mas informacion'}
+  ];
+  id = null;
+  lugar: any = {};
   constructor(private route: ActivatedRoute) {
     console.log(this.route.snapshot.params['id']);
     console.log(this.route.snapshot.queryParams['action2']);
     console.log(this.route.snapshot.queryParams['referer']);
-}
+    this.id = this.route.snapshot.params['id'];
+    this.lugar = (this.buscarLugar());
+  }
+  buscarLugar () {
+    return this.lugares.filter((lugar) => {return lugar.id == this.id})[0] || null;
+  }
 }
