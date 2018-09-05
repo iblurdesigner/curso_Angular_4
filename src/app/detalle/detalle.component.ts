d4ebd203747b800be0c923c4114f251f7115af5a
“iblurdesigner” <nevomen@gmail.comgit config --global user.emailgit config --global user.email nevomen@gmail.com>
2018-09-05 09:24:43 -0500

    Parametros tipo query

--- src/app/detalle/detalle.component.ts
+++ src/app/detalle/detalle.component.ts
@@ -8,5 +8,7 @@ import {ActivatedRoute} from '@angular/router';
 export class DetalleComponent {
   constructor(private route: ActivatedRoute) {
     console.log(this.route.snapshot.params['id']);
+    console.log(this.route.snapshot.queryParams['action2']);
+    console.log(this.route.snapshot.queryParams['referer']);
 }
 }
